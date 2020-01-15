aaa66ebab61fe4beb9687bf75fdbeadc2c7a3947
reidsteven75 <reidsteven75@gmail.com>
2020-01-14 20:50:06 -0700

    hologram test

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from Hologram.HologramCloud import HologramCloud
+credentials = {'devicekey': '12345a7b'}
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,3 +10,14 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
+    cloud = HologramCloud(dict(), network='cellular')
+    print('HOLOGRAM')
+    print cloud.version # Prints 0.7.0
+    print cloud.network_type # Prints either 'Network Agnostic Mode' or 'Cellular'
+
+    hologram = HologramCloud(credentials, network='cellular')
+
+    print('sending message...')
+    recv = cloud.sendMessage("hi there!", topics=["TOPIC1","TOPIC2"])
+
