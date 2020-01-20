cb9eb87f407ba8ac545e2f36ac05b4d3620ab51e
reidsteven75 <reidsteven75@gmail.com>
2020-01-20 16:38:19 -0700

    update

--- src/main.py
+++ src/main.py
@@ -1,15 +1,7 @@
-from flask import Flask
 from Hologram.HologramCloud import HologramCloud
 credentials = {'devicekey': '12345a7b'}
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
 
     cloud = HologramCloud(dict(), network='cellular')
     print('HOLOGRAM')
